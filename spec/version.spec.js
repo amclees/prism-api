a01db3deaedfa87446a4a06bd7df3d5df168c32a
amclees <andrewmclees5@gmail.com>
2017-10-13 14:34:32 -0700

    Start on version model spec

---
+++ spec/version.spec.js
@@ -0,0 +1,67 @@
+'use strict';
+
+require('dotenv').config();
+
+const winston = require('winston');
+winston.level = process.env.LOG_LEVEL;
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect(process.env.DB_HOST, {useMongoClient: true});
+
+//const Version = require('../models/version.model');
+const User = require('../models/user.model');
+
+describe('The version model', () => {
+  const user = new User({
+    username: 'username',
+    email: 'email@example.com',
+    name: {
+      first: 'first name',
+      last: 'last name'
+    },
+    internal: true,
+    root: false
+  });
+  let versionData;
+
+  // TODO: Write fixture to disk if it does not exist
+  beforeAll(() => {});
+
+  beforeEach((done) => {
+    versionData = {
+      filePath: './spec/fixtures/test.txt',
+      versionMessage: 'First test document',
+      uploader: user._id
+    };
+    Version.remove({}, function(error) {
+      if (error) {
+        winston.log('error', `Error removing all Version documents from database between tests: ${error.message}`);
+      }
+      done();
+    });
+  });
+
+  it('allows creating a version with valid data', (done) => {
+    const version = new Version(versionData);
+
+    version.save((error) => {
+      expect(error).toBe(null);
+      done();
+    });
+  });
+
+  it('requires a version message', (done) => {
+    versionData.versionMessage = '';
+    const version = new Version(versionData);
+
+    version.save((error) => {
+      expect(error).not.toBe(null);
+      done();
+    });
+  });
+
+  it('stores the date when it was created', () => {});
+
+  it('salts and hashes its file', () => {});
+});
