3d8dccc0b0ab63c87f946dd879f1e310928c0bc1
amclees <andrewmclees5@gmail.com>
2017-10-12 22:12:37 -0700

    Add password storage to user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const settings = require('../config/settings.js');
 
 const requiredString = error => {
   return {type: String, required: [true, error]};
@@ -13,14 +16,30 @@ const userSchema = new mongoose.Schema({
     first: requiredString('Missing first name'),
     last: requiredString('Missing last name')
   },
-  internal: {type: Boolean, required: [true, 'Missing internal boolean']},
+  internal: {
+    type: Boolean,
+    required: [true, 'Missing internal boolean']
+  },
+  root: {
+    type: Boolean,
+    default: false
+  },
   cas: {},
-  password_hash: String,
-  salt: String
+  passwordHash: String
 });
 
 userSchema.methods = {
-
+  setPassword: function(passwordPlaintext) {
+    return bcrypt.hash(passwordPlaintext, settings.saltRounds).then((passwordHash) => {
+      this.passwordHash = passwordHash;
+      return passwordHash;
+    });
+  },
+  comparePassword: function(passwordPlaintext) {
+    return bcrypt.compare(passwordPlaintext, this.passwordHash).then((same) => {
+      return same;
+    });
+  }
 };
 
 userSchema.statics = {
