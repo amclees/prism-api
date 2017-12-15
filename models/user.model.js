80aa61552a2f2204dc074b17e1c682f73e30cf7b
Andrew McLees <andrewmclees5@gmail.com>
2017-12-15 12:07:04 -0800

    Remove unnecessary winston log level changes

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,6 @@
 'use strict';
 
-require('dotenv').config();
-
 const winston = require('winston');
-winston.level = process.env.LOG_LEVEL;
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
