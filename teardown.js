38dad6a18dc74513bd8cfbcfe3a92e8963659496
Andrew McLees <andrewmclees5@gmail.com>
2017-12-15 11:37:38 -0800

    Add error handling to teardown

--- teardown.js
+++ teardown.js
@@ -1,19 +1,26 @@
+const winston = require('winston');
+
+const disconnectErrorHandler = (err) => {
+  winston.error('Could not diconnect from MongoDB, error:', err);
+  process.exit(0);
+};
+
 module.exports = function(disconnect) {
   process.once('SIGUSR2', () => {
     disconnect('received SIGUSR2 signal (likely nodemon restart)', () => {
       process.kill(process.pid, 'SIGUSR2');
-    });
+    }, disconnectErrorHandler);
   });
 
   process.on('SIGINT', () => {
     disconnect('received interrupt signal', () => {
       process.exit(0);
-    });
+    }, disconnectErrorHandler);
   });
 
   process.on('SIGTERM', () => {
     disconnect('received terminate signal', () => {
       process.exit(0);
-    });
+    }, disconnectErrorHandler);
   });
 };
