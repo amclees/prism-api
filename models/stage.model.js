ce277ca003c11dc53ca91ff6b36453a8e5dc4bfb
amclees <andrewmclees5@gmail.com>
2017-12-14 20:39:55 -0800

    Add completion date estimation to Stage model

--- models/stage.model.js
+++ models/stage.model.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const mongoose = require('mongoose');
+require('dotenv').config();
 
+const winston = require('winston');
+winston.level = process.env.LOG_LEVEL;
+
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+// For use in verification later on
 const nodeType = {
   startDate: Date,
   finishDate: Date,
@@ -9,6 +16,7 @@ const nodeType = {
   finishDateOverriden: Boolean,
   finalized: Boolean,
   email: Object,
+  document: mongoose.Schema.Types.ObjectId,
   prerequisites: {
     type: [mongoose.Schema.Types.ObjectId],
     default: []
@@ -17,7 +25,7 @@ const nodeType = {
 
 const stageSchema = new mongoose.Schema({
   endNodes: {
-    type: [nodeType],
+    type: [mongoose.Schema.Types.ObjectId],
     default: []
   },
   nodes: {
@@ -26,4 +34,51 @@ const stageSchema = new mongoose.Schema({
   }
 });
 
+stageSchema.pre('init', function(next) {
+  let safety = 1000;
+  const fillNodeDate = (nodeId) => {
+    if (safety-- <= 0) {
+      winston.log('error', 'Exceeded limit to node count in calculating date estimates in Stage');
+      return 0;
+    }
+
+    const node = this.nodes[nodeId];
+
+    if (node.finishDateOverriden) {
+      node.recalculated = true;
+      return;
+    }
+
+    let soonestStartDate = new Date(Number.MAX_VALUE);
+    if (nodeId.prerequisites.length === 0) {
+      soonestStartDate = new Date(Date.now());
+    } else {
+      for (let prerequisite of _.map(node.prerequisites, (prerequisiteId) => {
+             return this.nodes[prerequisiteId];
+           })) {
+        if (!prerequisite.recalculated) {
+          fillNodeDate(prerequisite);
+        }
+        if (prerequisite.finishDate < soonestStartDate) {
+          soonestStartDate = prerequisite.finishDate;
+        }
+      }
+    }
+    node.finishDate = offsetDate(soonestStartDate, node.completionEstimate);
+    node.recalculated = true;
+  };
+
+  for (let endNode of this.endNodes) {
+    fillNodeDate(endNode);
+  }
+
+  next();
+});
+
 module.exports = mongoose.model('Stage', stageSchema);
+
+function offsetDate(date, days) {
+  const dateToAdjust = new Date(this.valueOf());
+  dateToAdjust.setDate(dateToAdjust.getDate() + days);
+  return dateToAdjust;
+}
