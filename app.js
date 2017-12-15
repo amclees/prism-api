ce7d828ba77bfc7cf86294d3902ddd7af5b79e46
amclees <andrewmclees5@gmail.com>
2017-12-15 10:51:49 -0800

    Log from Morgan to Winston and log to files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
+require('./log.js');
 const winston = require('winston');
-winston.level = process.env.LOG_LEVEL;
 
 const express = require('express');
 const path = require('path');
@@ -18,7 +18,7 @@ const routes = require('./routes');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan(process.env.MORGAN_MODE ? process.env.MORGAN_MODE : 'combined'));
+app.use(morgan(process.env.MORGAN_MODE ? process.env.MORGAN_MODE : 'combined', {stream: winston.infoStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
