532d3f1fe90d55b1d6c7eeb4f635226dbed83b8c
amclees <andrewmclees5@gmail.com>
2017-12-18 10:53:14 -0800

    Fix stage model initializer

--- models/stage.model.js
+++ models/stage.model.js
@@ -6,7 +6,6 @@ const winston = require('winston');
 winston.level = process.env.LOG_LEVEL;
 
 const mongoose = require('mongoose');
-const _ = require('lodash');
 
 // For use in verification later on
 const nodeType = {
@@ -34,7 +33,8 @@ const stageSchema = new mongoose.Schema({
   }
 });
 
-stageSchema.pre('init', function(next) {
+stageSchema.post('init', function() {
+  console.log('filled');
   let safety = 1000;
   const fillNodeDate = (nodeId) => {
     if (safety-- <= 0) {
@@ -44,41 +44,40 @@ stageSchema.pre('init', function(next) {
 
     const node = this.nodes[nodeId];
 
-    if (node.finishDateOverriden) {
+    if (node.finishDateOverriden || node.finalized) {
       node.recalculated = true;
       return;
     }
 
-    let soonestStartDate = new Date(Number.MAX_VALUE);
-    if (nodeId.prerequisites.length === 0) {
-      soonestStartDate = new Date(Date.now());
+    let prerequisiteFinishDate = new Date(0);
+    if (node.prerequisites.length === 0) {
+      prerequisiteFinishDate = new Date(node.startDate);
     } else {
-      for (let prerequisite of _.map(node.prerequisites, (prerequisiteId) => {
-             return this.nodes[prerequisiteId];
-           })) {
+      for (let prerequisiteId of node.prerequisites) {
+        const prerequisite = this.nodes[prerequisiteId];
         if (!prerequisite.recalculated) {
-          fillNodeDate(prerequisite);
+          fillNodeDate(prerequisiteId);
         }
-        if (prerequisite.finishDate < soonestStartDate) {
-          soonestStartDate = prerequisite.finishDate;
+        if (prerequisite.finishDate > prerequisiteFinishDate) {
+          prerequisiteFinishDate = prerequisite.finishDate;
         }
       }
     }
-    node.finishDate = offsetDate(soonestStartDate, node.completionEstimate);
+
+    node.startDate = prerequisiteFinishDate;
+    node.finishDate = offsetDate(prerequisiteFinishDate, node.completionEstimate);
     node.recalculated = true;
   };
 
   for (let endNode of this.endNodes) {
     fillNodeDate(endNode);
   }
-
-  next();
 });
 
 module.exports = mongoose.model('Stage', stageSchema);
 
 function offsetDate(date, days) {
-  const dateToAdjust = new Date(this.valueOf());
+  const dateToAdjust = new Date(date.getTime());
   dateToAdjust.setDate(dateToAdjust.getDate() + days);
   return dateToAdjust;
 }
