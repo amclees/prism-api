48685cfb314a2d4a644f4ceb02c6da7718949835
amclees <andrewmclees5@gmail.com>
2017-12-18 11:46:22 -0800

    Add program endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ module.exports = [
   require('./group.route'),
   require('./college.route'),
   require('./department.route'),
+  require('./program.route'),
   require('./test_endpoint')
 ];
---
+++ routes/program.route.js
@@ -0,0 +1,59 @@
+const winston = require('winston');
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const Program = mongoose.model('Program');
+
+router.route('/program/:program_id')
+    .get(function(req, res, next) {
+      Program.findById(req.params.program_id).then(function(program) {
+        res.json(program);
+      }, function(err) {
+        err.status = 404;
+        next(err);
+      });
+    })
+    .patch(function(req, res, next) {
+      Program.findByIdAndUpdate(req.params.program_id, {$set: req.body}, {new: true, runValidators: true}).then(function(updatedProgram) {
+        res.json(updatedProgram);
+        winston.info(`Updated program with id ${req.params.program_id}`);
+      }, function(err) {
+        next(err);
+      });
+    })
+    .delete(function(req, res, next) {
+      Program.findByIdAndRemove(req.params.program_id).then(function(removedDocument) {
+        if (removedDocument) {
+          res.sendStatus(204);
+          winston.info(`Removed program with id ${req.params.program_id}`);
+        } else {
+          res.sendStatus(404);
+          winston.info(`Tried to remove nonexistent program with id ${req.params.program_id}`);
+        }
+      }, function(err) {
+        next(err);
+      });
+    });
+
+router.route('/program').post(function(req, res, next) {
+  Program.create(req.body).then(function(newProgram) {
+    res.status(201);
+    res.json(newProgram);
+    winston.info(`Created program with id ${newProgram._id}`);
+  }, function(err) {
+    next(err);
+    winston.info('Failed to create program with body:', req.body);
+  });
+});
+
+router.get('/programs', function(req, res, next) {
+  Program.find().exec().then(function(programs) {
+    res.json(programs);
+  }, function(err) {
+    next(err);
+    winston.error('Error fetching all programs:', err);
+  });
+});
+
+module.exports = router;
