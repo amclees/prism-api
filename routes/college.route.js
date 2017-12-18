55190bad42422b6b66dcce5f69a7d1586a56c301
amclees <andrewmclees5@gmail.com>
2017-12-18 11:31:23 -0800

    Add college endpoints

--- routes/college.route.js
+++ routes/college.route.js
@@ -1,14 +1,58 @@
+const winston = require('winston');
 const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
 const College = mongoose.model('College');
 
-router.get('/colleges', function(req, res) {
-  College.find({}, function(err, college){
-    if (err)
-      res.send(err);
-    res.json(college);
+router.route('/college/:college_id')
+    .get(function(req, res, next) {
+      College.findById(req.params.college_id).then(function(college) {
+        res.json(college);
+      }, function(err) {
+        err.status = 404;
+        next(err);
+      });
+    })
+    .put(function(req, res, next) {
+      College.findByIdAndUpdate(req.params.college_id, {$set: {name: req.body.name, dean: req.body.dean}}, {new: true, runValidators: true}).then(function(updatedCollege) {
+        res.json(updatedCollege);
+        winston.info(`Updated college with id ${req.params.college_id}`);
+      }, function(err) {
+        next(err);
+      });
+    })
+    .delete(function(req, res, next) {
+      College.findByIdAndRemove(req.params.college_id).then(function(removedDocument) {
+        if (removedDocument) {
+          res.sendStatus(204);
+          winston.info(`Removed college with id ${req.params.college_id}`);
+        } else {
+          res.sendStatus(404);
+          winston.info(`Tried to remove nonexistent college with id ${req.params.college_id}`);
+        }
+      }, function(err) {
+        next(err);
+      });
+    });
+
+router.route('/college').post(function(req, res, next) {
+  College.create(req.body).then(function(newCollege) {
+    res.status(201);
+    res.json(newCollege);
+    winston.info(`Created college with id ${newCollege._id}`);
+  }, function(err) {
+    next(err);
+    winston.info('Failed to create college with body:', req.body);
+  });
+});
+
+router.get('/colleges', function(req, res, next) {
+  College.find().exec().then(function(colleges) {
+    res.json(colleges);
+  }, function(err) {
+    next(err);
+    winston.error('Error fetching all colleges:', err);
   });
 });
 
