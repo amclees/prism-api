22c38577c3e4998f7c4cd1568ca0cd8a3823b3ad
amclees <andrewmclees5@gmail.com>
2017-12-27 14:19:33 -0800

    Add helper methods to document schema for revisions

--- models/document.model.js
+++ models/document.model.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const winston = require('winston');
+
+const fileUtils = require('../lib/file_utils.js');
 
 const notDeleted = function() {
   return !this.deleted;
@@ -12,12 +15,7 @@ const documentSchema = new mongoose.Schema({
     required: true
   },
   currentRevision: {
-    type: Number,
-    validate: {
-      validator: function(value) {
-        return value === undefined || (value > 0 && value < this.allRevisions.length);
-      }
-    }
+    type: Number
   },
   revisions: {
     type: [{
@@ -35,9 +33,11 @@ const documentSchema = new mongoose.Schema({
       },
       uploader: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: notDeleted
       },
-      deleted: Boolean
+      deleted: Boolean,
+      template: Boolean
     }],
     default: []
   },
@@ -49,5 +49,64 @@ const documentSchema = new mongoose.Schema({
     default: []
   }
 });
+
+documentSchema.methods.setRevision = function(index) {
+  try {
+    winston.debug('Setting document currentRevision');
+    if (index === undefined) {
+      winston.info('Setting revision to undefined');
+      this.currentRevision = index;
+      return true;
+    } else if (index >= 0 && index < this.revisions.length && !this.revisions[index].deleted) {
+      winston.info(`Setting revision to index ${index}`);
+      this.currentRevision = index;
+      return true;
+    } else {
+      winston.error(`Attempted to set revision to ${index} but it was invalid`);
+      return false;
+    }
+  } catch (err) {
+    winston.error('Error setting revision', err);
+    return false;
+  }
+};
+
+documentSchema.methods.addRevision = function(message, filePath, uploader) {
+  this.revisions.push({
+    'message': message,
+    'filePath': filePath,
+    'uploader': uploader
+  });
+};
+
+documentSchema.methods.deleteRevision = function(toDelete) {
+  const index = Number.parseInt(toDelete);
+  return new Promise((resolve, reject) => {
+    if (isNaN(index)) {
+      reject(new Error('currentRevision must be a number'));
+      return;
+    }
+    if (this.revisions[index].template) {
+      reject(new Error('Attempted to delete template revision'));
+      return;
+    }
+    fileUtils.deleteFile(this.revisions[index].filePath).then(() => {
+      if (this.currentRevision === index) {
+        this.currentRevision = undefined;
+      }
+      this.revisions[index] = {
+        deleted: true
+      };
+      winston.info('Successfully deleted version file');
+      this.save().then(function() {
+        resolve();
+      }, function(err) {
+        reject(err);
+      });
+    }, function(err) {
+      reject(err);
+    });
+  });
+};
 
 module.exports = mongoose.model('Document', documentSchema);
