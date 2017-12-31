da3b1f10b04d55e1a6c64f58ade990504583b123
amclees <andrewmclees5@gmail.com>
2017-12-31 11:41:35 -0800

    Add Action model

---
+++ models/action.model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const actionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  // Type and Object are for use by the client-side to provide shortcuts to affected objects
+  // Type indicates what type of object the action affects
+  type: {
+    type: String,
+    default: 'none'
+  },
+  // Object is the object affected by the action
+  object: mongoose.Schema.Types.ObjectId
+});
+
+actionSchema.path('type').validate({
+  validator: function(type) {
+    return ['college', 'comment', 'department', 'document', 'group', 'program', 'review', 'review', 'user'].includes(type);
+  },
+  isAsync: false,
+  message: 'Action must have a valid type, see list of action types'
+});
+
+module.exports = mongoose.model('Action', actionSchema);
