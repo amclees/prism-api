a1424b7c00e5de316dd5d6883f2c750d8aea3a31
Ben Solis <bsolis9@calstatela.edu>
2018-01-05 14:05:05 -0800

    Clean up the POST, GET, and PUT endpoints for user crud operations
    Test for the POST, GET, and PUT user crud operations. Make changes
    to code accordingly and add '/users' endpoint for retrieving list
    of users.

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,63 +9,44 @@ const _ = require('lodash');
 const winston = require ('winston');
 winston.level = process.env.LOG_LEVEL || 'info';
 
-router.param('user_id', function(req, res, next, id) {
-  req.id = id;
-  try {
-    User.findById(id).then((user) => {
-      req.user = user;
-      next();
-    }, () => {
-      next();
-    });
-  } catch (err) {
-    // Invalid object id
-    next();
-  }
-});
-
+// router.param('user_id', function(req, res, next, id) {
+//   req.id = id;
+//   try {
+//     User.findById(id).then((user) => {
+//       req.user = user;
+//       next();
+//     }, () => {
+//       next();
+//     });
+//   } catch (err) {
+//     // Invalid object id
+//     next();
+//   }
+// });
 router.route('/user/:user_id?')
   .all(function(req, res, next) {
-    var awesome_instanc = new User({
-    "username": "Drdsdf009",
-    "email": "benjairsolis@yahoo.com",
-    "internal": true,
-    "enabled": true,
-    "root": false,
-    "name": {
-        "first": "Diucdsgf",
-        "last": "Sudcd"
-    }
-});
-
-    // Save the new model instance, passing a callback
-    awesome_instance.save(function (err) {
-      if (err) return handleError(err);
-      // saved!
-    });
     // Access control
     next();
   })
   .get(function(req, res, next) {
-    if (req.user) {
-      res.json(_.omit(req.user, ['passwordHash', 'enabled']));
-    } else {
-      res.sendStatus(404);
-    }
+    User.findById(req.params.user_id).then((user) => {
+      res.json(user);
+    }, function(err) {
+      next(err);
+    });
   })
   // Update only what's allowed
-  .put(function(req, res, next) {
-    if(req.user.username != req.params.username) {
+  .put(function(req, res) {
+    if(req.user._id != req.params.user_id) { //ANDREW CHECK THIS LINE FOR CORRECTIONS - BEN
       res.sendStatus(403);
+    } else {
+      const update = _.pick(req.body, ['username', 'email', 'name']);
+      User.findByIdAndUpdate(req.params.user_id, {$set: update}, {new: true, runValidators: true}).then(function(updatedUser) {
+        res.json(updatedUser);
+      }, function(err) {
+        res.sendStatus(400);
+      });
     }
-    // {username: req.body.username, email: req.body.email, name: req.body.name}}
-    const update = _.pick(req.body, ['username', 'email', 'name']);
-    User.findOneAndUpdate({_id: req.id}, {$set: update}, {new: true,
-        runValidators: true}).then(function(updatedUser) {
-      res.json(updatedUser);
-    }, function(err) {
-      res.sendStatus(400);
-    });
   })
   .post(function(req, res, next) {
     const newUser = _.omit(req.body, 'password');
@@ -74,7 +55,7 @@ router.route('/user/:user_id?')
       winston.info(`User ${createdUser.username} created.`);
       res.json(createdUser.excludeFields());
     }, function(err) {
-      next(new Error(err));
+      next(err);
     });
   })
   .delete(function(req, res, next) {
@@ -89,6 +70,14 @@ router.route('/user/:user_id?')
     // } else {
     //   res.sendStatus(404);
     // }
-  })
+  });
+
+  router.use('/users', function(req, res, next) {
+    User.find().then((users) => {
+      res.json(users);
+    }, function(err) {
+      next(err);
+    });
+  });
 
 module.exports = router;
