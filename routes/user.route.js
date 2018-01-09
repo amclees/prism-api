389ee0ee9c7e9d291fd4de5de82a8017b5a31306
amclees <andrewmclees5@gmail.com>
2018-01-09 11:44:50 -0800

    Finish basic user CRUD functionality

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,81 +1,72 @@
+const _ = require('lodash');
+const winston = require('winston');
 const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-const _ = require('lodash');
-
-const winston = require('winston');
-winston.level = process.env.LOG_LEVEL || 'info';
-
-// router.param('user_id', function(req, res, next, id) {
-//   req.id = id;
-//   try {
-//     User.findById(id).then((user) => {
-//       req.user = user;
-//       next();
-//     }, () => {
-//       next();
-//     });
-//   } catch (err) {
-//     // Invalid object id
-//     next();
-//   }
-// });
-router.route('/user/:user_id?')
-    .all(function(req, res, next) {
-      // Access control
-      next();
-    })
+router.route('/user/:user_id')
     .get(function(req, res, next) {
       User.findById(req.params.user_id).then((user) => {
-        res.json(user);
+        if (user === null || user.disabled) {
+          next();
+          return;
+        }
+        if (req.user._id.equals(mongoose.Types.ObjectId(req.params.user_id))) {
+          res.json(user.excludeFieldsWithConfig());
+        } else {
+          res.json(user.excludeFields());
+        }
       }, function(err) {
         next(err);
       });
     })
-    // Update only what's allowed
-    .put(function(req, res) {
-      if (req.user._id != req.params.user_id) {  //ANDREW CHECK THIS LINE FOR CORRECTIONS - BEN
-        res.sendStatus(403);
-      } else {
-        const update = _.pick(req.body, ['username', 'email', 'name']);
-        User.findByIdAndUpdate(req.params.user_id, {$set: update}, {new: true, runValidators: true}).then(function(updatedUser) {
-          res.json(updatedUser);
-        }, function(err) {
-          res.sendStatus(400);
-        });
-      }
-    })
-    .post(function(req, res, next) {
-      const newUser = _.omit(req.body, 'password');
-      User.create(newUser).then(function(createdUser) {
-        createdUser.setPassword(req.body.password);
-        winston.info(`User ${createdUser.username} created.`);
-        res.json(createdUser.excludeFields());
+    .patch(function(req, res, next) {
+      // Since only admins will be able to patch other users after access control, checking for disabled is not necessary
+      const update = _.pick(req.body, ['username', 'email', 'name', 'config']);
+      User.findByIdAndUpdate(req.params.user_id, {$set: update}, {new: true, runValidators: true}).then(function(updatedUser) {
+        res.json(updatedUser.excludeFieldsWithConfig());
       }, function(err) {
         next(err);
       });
     })
     .delete(function(req, res, next) {
-      //if (req.user) {
-      // Deletion will disable users not delete them
-      User.findOneAndUpdate({_id: req.param.id}, {$set: {disabled: true}}, {new: true,
-                                                                            runValidators: true})
-          .then(function(disabledUser) {
-            res.sendStatus(204);
-          }, function() {
-            next(new Error('Error disabling user'));
-          });
-      // } else {
-      //   res.sendStatus(404);
-      // }
+      User.findByIdAndUpdate(req.params.user_id, {$set: {disabled: true}}, {new: true, runValidators: true}).then(function(disabledUser) {
+        if (disabledUser) {
+          res.sendStatus(204);
+        } else {
+          res.sendStatus(404);
+        }
+      }, function(err) {
+        next(err);
+      });
     });
+
+router.post('/user', function(req, res, next) {
+  const newUser = _.pick(req.body, ['username', 'email', 'name', 'internal']);
+  User.create(newUser).then(function(createdUser) {
+    createdUser.setPassword(req.body.password).then(function() {
+      createdUser.save().then(function() {
+        winston.info(`User ${createdUser.username} created.`);
+        res.json(createdUser.excludeFields());
+      }, function(err) {
+        next(err);
+      });
+    }, function(err) {
+      next(err);
+    });
+  }, function(err) {
+    next(err);
+  });
+});
 
 router.use('/users', function(req, res, next) {
   User.find().then((users) => {
-    res.json(users);
+    // Once groups are attached to req, should allow admins to see disabled users
+    res.json(_.map(_.reject(users, 'disabled'), function(user) {
+      return user.excludeFields();
+    }));
   }, function(err) {
     next(err);
   });
