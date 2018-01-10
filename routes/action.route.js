c72512dcbc90eaadd3928f62f740665900527aab
amclees <andrewmclees5@gmail.com>
2018-01-10 12:02:22 -0800

    Filter result of actions endpoint to show only own actions for non-admins

--- routes/action.route.js
+++ routes/action.route.js
@@ -1,15 +1,20 @@
 const winston = require('winston');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
 const Action = mongoose.model('Action');
 
+const access = require('../lib/access');
 const settings = require('../lib/config/settings');
 
-router.get('/actions', function(req, res, next) {
-  // Once authentication is done, need to customize the query for non-admins
+router.get('/actions', access.attachGroups, function(req, res, next) {
   const query = {};
+  if (!req.user.root && _.map(req.groups, 'name').indexOf('Administrators') === -1) {
+    query.user = req.user._id;
+  }
+
   const page = req.query.page ? req.query.page : 0;
   Action.find(query).sort({date: 'desc'}).limit(settings.actionsPerPage).skip(settings.actionsPerPage * page).populate('user').exec().then(function(actions) {
     const excludedActions = [];
