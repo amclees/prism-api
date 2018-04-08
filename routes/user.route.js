cd9dfb0148cd6f8ef302a91713a009f3b041718b
amclees <andrewmclees5@gmail.com>
2018-04-08 12:16:40 -0700

    Allow viewing disabled user data
    Needed to view external reviewer data

--- routes/user.route.js
+++ routes/user.route.js
@@ -12,7 +12,7 @@ const tokenCache = require('../lib/token_cache');
 router.route('/user/:user_id')
     .get(function(req, res, next) {
       User.findById(req.params.user_id).then((user) => {
-        if (user === null || user.disabled) {
+        if (user === null) {
           next();
           return;
         }
